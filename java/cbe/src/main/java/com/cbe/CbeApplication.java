2a0b74729e3627f9ef8a41a11d32cd5e846b5bdd
Leo Gutiérrez Ramírez <leogutierrezramirez@gmail.com>
2019-01-05 16:20:01 -0700

    adding project skeletons

---
+++ java/cbe/src/main/java/com/cbe/CbeApplication.java
@@ -0,0 +1,14 @@
+package com.cbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CbeApplication.class, args);
+	}
+
+}
+
