fe7609541346a56b264b9c78dc9d84a4376012a5
Leo <leogutierrezramirez@gmail.com>
2019-01-20 22:58:53 -0700

    tried to add swagger

---
+++ java/cbe/src/main/java/com/cbe/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.cbe.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurationSupport {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+}
--- java/cbe/src/main/java/com/cbe/domain/Person.java
+++
@@ -1,4 +0,0 @@
-package com.cbe.domain;
-
-public class Person {
-}
